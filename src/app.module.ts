ff8ba370eac7774e888625bcd1a023c6b8534af4
codefher-windows <fernando.mendoza@segip.gob.bo>
2024-10-14 12:29:47 -0400

    my first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { HelloController } from './hello/hello.controller';
+
+@Module({
+  imports: [ProjectsModule, TasksModule, AuthModule, UsersModule],
+  controllers: [HelloController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Controller('/tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(@Query() query: any) {
+    console.log(query);
+    return this.tasksService.getTasks();
+  }
+
+  @Get('/:id')
+  getTask(@Param('id') id: string) {
+    console.log(id);
+    return this.tasksService.getTask(parseInt(id));
+  }
+
+  @Post()
+  createTasks(@Body() task: CreateTaskDto) {
+    return this.tasksService.createTasks(task);
+  }
+
+  @Put()
+  updateTask(@Body() task: UpdateTaskDto) {
+    return this.tasksService.updateTask(task);
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
+
+  @Patch()
+  updateTaskStatus() {
+    return this.tasksService.updateTaskStatus();
+  }
+}
